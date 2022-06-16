4dcfd220f9eafcc569ee0887cb42e9c96c4e3343
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-06-16 11:56:37 +0200

    Pages finies + navbar

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import TopBar from './components/TopBar';
 import About from './components/About';
 import Contact from './components/Contact';
+import NavBar from './components/NavBar';
+import Policy from './components/Policy';
 
 function App() {
   return (
     <BrowserRouter>
       <TopBar />
+      <NavBar />
       <Routes>
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/policy" element={<Policy />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Policy.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Container } from 'react-bootstrap';
+
+const Policy = () => {
+  return (
+    <Container style={{marginTop: '50px'}}>
+        <h2>CONDITIONS GENERALES DE VENTE LA BOITE A PIZZA</h2>
+        <h5>DEFINITIONS</h5>
+        <p>Dans les présentes Conditions Générales de Vente, les mots et expressions suivants auront les significations suivantes :</p>
+        <p>« Client » désigne le ou les internaute(s) naviguant sur le Site et effectuant une commande par téléphone ou un achat en ligne de Produits.</p>
+        <p>« CGV » désigne les présentes Conditions Générales de Vente de Produits sur le Site.</p>
+        <p>« LA BOITE A PIZZA » désigne, selon le cas, soit la société d’exploitation du restaurant sous enseigne LA BOITE A PIZZA sélectionné par le Client et auprès de laquelle la commande sera enregistrée et dont les coordonnées seront indiquées sur l’e-mail confirmant la commande et sur le ticket de caisse délivré par LA BOITE A PIZZA lors du retrait ou de la livraison des Produits, soit, de manière générale, les restaurants sous enseigne LA BOITE A PIZZA.</p>
+        <p>« Produit(s) » désigne le(s) produit(s) proposé(s) à la vente sur le Site.</p>
+        <h5>PORTEE DES CONDITIONS GENERALES DE VENTE</h5>
+        <p>LA BOITE A PIZZA a pour activité la vente de pizzas, et autres produits d’accompagnement, en livraison, sur place et en vente à emporter.</p>
+        <p>Les présentes CGV sont accessibles à tout moment sur le Site et s’appliquent à toutes les commandes de Produits passées par le Client sur le Site ou par téléphone auprès de LA BOITE A PIZZA. En passant commande d'un Produit, le Client reconnaît expressément avoir pris connaissance des CGV et les avoir acceptées sans restriction ni réserve, en cochant la case prévue à cet effet, et renonce, notamment, à se prévaloir de toute autre version ou document contradictoire, et notamment les conditions applicables pour les ventes en magasins. L’acceptation des CGV n'est en aucune façon conditionnée par une signature manuscrite de la part du Client.</p>
+        <p>LA BOITE A PIZZA se réserve le droit de mettre à jour à tout moment les CGV. En cas de modification, il sera appliqué, à chaque commande, les CGV en vigueur au jour de la commande de Produits. Le Client est invité à consulter régulièrement les CGV afin de se tenir informé des évolutions les plus récentes.</p>
+        <p>Les données enregistrées par le Site constituent la preuve de l’ensemble des transactions conclues avec le Client.</p>
+        <h5>ACCEPTATION DES CGV</h5>
+        <p>Le Client déclare avoir pris connaissance et accepté les présentes CGV avant la passation de sa Commande. Toute Commande d'un Produit sur le Site matérialise donc de la part du Client l'acceptation des présentes CGV.</p>
+        <h5>TERRITOIRE – ZONES DE LIVRAISON</h5>
+        <p>Le Client commandant par téléphone ou via le Site et qui souhaite être livré doit fournir une adresse de livraison valide située dans une zone de livraison d’un établissement LA BOITE A PIZZA. La liste actualisée des restaurants LA BOITE A PIZZA et des villes desservies par LA BOITE A PIZZA est disponible sur le Site. Si l’adresse indiquée n’est pas desservie, le Client en est informé avant la commande.</p>
+    </Container>
+  )
+}
+
+export default Policy
