28bfff170b97aaee9fe08f81e2fff4022aca12ae
Nicolas TAPIA <ntapia.dev@gmail.com>
2022-06-20 16:54:52 +0200

    PayPalButtons

--- src/App.js
+++ src/App.js
@@ -13,29 +13,38 @@ import Policy from './components/Policy';
 import Register from './components/Register';
 import RequireAuth from "./components/RequireAuth"
 import { AuthProvider } from './context/AuthProvider';
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 function App() {
   const [cartLength, setCartLength] = useState(localStorage.length);
+  const initialOptions = {
+    "client-id": "test",
+    currency: "USD",
+    intent: "capture",
+    "data-client-token": "abc123xyz==",
+  };
 
   return (
-    <BrowserRouter>
-      <AuthProvider>
-        <TopBar />
-        <NavBar cartLength={cartLength} />
-        <Routes>
-          <Route path="/" element={<HomeScreen setCartLength={setCartLength} />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/policy" element={<Policy />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-          <Route element={<RequireAuth />}>
-            <Route path="/cart" element={<Cart setCartLength={setCartLength}/>} />
-            <Route path="/paypal" element={<Paypal />} />
-          </Route>
-        </Routes>
-      </AuthProvider>
-    </BrowserRouter>
+    <PayPalScriptProvider options={initialOptions}>
+      <BrowserRouter>
+        <AuthProvider>
+          <TopBar />
+          <NavBar cartLength={cartLength} />
+          <Routes>
+            <Route path="/" element={<HomeScreen setCartLength={setCartLength} />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/policy" element={<Policy />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/login" element={<Login />} />
+            <Route element={<RequireAuth />}>
+              <Route path="/cart" element={<Cart setCartLength={setCartLength} />} />
+              <Route path="/paypal" element={<Paypal />} />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
+    </PayPalScriptProvider>
   );
 }
 
--- src/components/Paypal.jsx
+++ src/components/Paypal.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Container } from 'react-bootstrap';
 import { useLocation } from 'react-router-dom';
+import { usePayPalScriptReducer, PayPalButtons } from "@paypal/react-paypal-js";
 
 const Paypal = () => {
     const location = useLocation();
+
+    const [{ isPending }] = usePayPalScriptReducer();
 
     return (
         <Container>
             <h1>Time to pay</h1>
             <p>{location.state.totalPrice}€ for {location.state.totalOrders} pizzas.</p>
+            {isPending ? <div className="spinner" /> : null}
+            {/* <PayPalButtons /> */}
         </Container>
     )
 }
